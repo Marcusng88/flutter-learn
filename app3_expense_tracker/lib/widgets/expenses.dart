39977468ce85051ccbe0fab41d060fc26e06fda4
Marcus Ng <ngzhengjie888@gmail.com>
2025-03-06 19:39:37 +0800

    add function to add new expenses

--- app3_expense_tracker/lib/widgets/expenses.dart
+++ app3_expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:app3_expense_tracker/widgets/new_expense.dart';
+
 import 'expenses_list/expenses_list.dart';
 import 'package:app3_expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
@@ -24,12 +26,17 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.leisure),
   ];
+
+  void _openAddExpenseOverlay(){
+    showModalBottomSheet(context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [IconButton(onPressed: _openAddExpenseOverlay, icon: Icon(Icons.add))],
       ),
       body: Column(
         children: [
