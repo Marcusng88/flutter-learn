5649b5fc639fa906db508830f789abbecad0593a
Marcus Ng <ngzhengjie888@gmail.com>
2025-03-14 17:56:46 +0800

    added some theming

--- app3_expense_tracker/lib/main.dart
+++ app3_expense_tracker/lib/main.dart
@@ -1,12 +1,38 @@
 import 'package:app3_expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 21, 100, 174));
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
       home: Expenses(),
-    )
+    ),
   );
 }
-
--- app3_expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ app3_expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -3,10 +3,11 @@ import 'package:app3_expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses, required this.onRemovedExpense});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemovedExpense});
 
   final List<Expense> expenses;
-  final void Function (Expense expense) onRemovedExpense;
+  final void Function(Expense expense) onRemovedExpense;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,14 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
-        onDismissed: (direction){
+        background: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.error.withValues(alpha: 0.8),
+              borderRadius: BorderRadius.circular(8)),
+          margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal),
+        ),
+        onDismissed: (direction) {
           onRemovedExpense(expenses[index]);
         },
         child: ExpenseItem(expenses[index]),
